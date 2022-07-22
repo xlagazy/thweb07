130f89acd5bc6b26e22cd8507b13ed9a25022506
xlagazy <xlagazy@gmail.com>
2022-07-22 11:03:55 +0700

    second commit

--- resources/views/stock/liststock.blade.php
+++ resources/views/stock/liststock.blade.php
@@ -5,6 +5,7 @@
 @section('contents')
 
     <div class="ct_listuser">
+        <h1></h1>
         <div>
             <a href="{{URL::to('liststock')}}" class="text-decoration-none text-dark">
                 <h1 style="margin-bottom:2%">Stock</h1>
